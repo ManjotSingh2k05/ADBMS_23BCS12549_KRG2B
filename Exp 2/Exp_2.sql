653705923d252eea72a2702c99daeef69efe65f8
Manjot Singh <129639545+ManjotSingh2k05@users.noreply.github.com>
2025-07-30 12:03:30 +0530

    Add files via upload

---
+++ Exp 2/Exp_2.sql
@@ -0,0 +1,48 @@
+-- Organizational Hierarchy Explorer (Medium)
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Ename VARCHAR(100),
+    Department VARCHAR(100),
+    ManagerID INT
+);
+
+INSERT INTO Employee (EmpID, Ename, Department, ManagerID) VALUES
+(1, 'Alice', 'HR', NULL),
+(2, 'Bob', 'Finance', 1),
+(3, 'Charlie', 'IT', 1),
+(4, 'David', 'Finance', 2),
+(5, 'Eve', 'IT', 3),
+(6, 'Frank', 'HR', 1);
+
+SELECT E.Ename AS EmployeeName, E.Department AS EmployeeDept,M.Ename AS ManagerName,M.Department AS ManagerDept
+FROM Employee E
+LEFT JOIN Employee M ON E.ManagerID = M.EmpID;
+
+-- Financial Forecast Matching with Fallback Strategy (Hard)
+CREATE TABLE Year_tbl (ID INT,YEAR INT,NPV INT);
+
+CREATE TABLE Queries_tbl (ID INT,YEAR INT);
+
+INSERT INTO Year_tbl (ID, YEAR, NPV) VALUES
+(1, 2018, 100),
+(7, 2020, 30),
+(13, 2019, 40),
+(1, 2019, 113),
+(2, 2008, 121),
+(3, 2009, 12),
+(11, 2020, 99),
+(7, 2019, 0);
+
+INSERT INTO Queries_tbl (ID, YEAR) VALUES
+(1, 2019),
+(2, 2008),
+(3, 2009),
+(7, 2018),
+(7, 2019),
+(7, 2020),
+(13, 2019);
+
+SELECT Q.ID,Q.YEAR,ISNULL(Y.NPV, 0) AS NPV
+FROM Queries_tbl Q
+LEFT JOIN Year_tbl Y ON Q.ID = Y.ID AND Q.YEAR = Y.YEAR
+ORDER BY Q.ID, Q.YEAR;
